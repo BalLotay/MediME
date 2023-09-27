ae2fbbbff11d800e9a9423c6417e9520698aab41
dguo095 <dguo095@uottawa.ca>
2023-09-27 18:27:37 -0400

    Add getters and setters

--- Person.java
+++ Person.java
@@ -1,4 +1,4 @@
-public class Person { // Class created
+public class Person { 
 
     private String firstName;
     private String lastName;
@@ -17,5 +17,65 @@ public class Person { // Class created
         this.address = address;
         this.healthCardNumber = healthCardNumber;
     }
+
+    public void setFirstName(String firstName) {
+		this.firstName = firstName;
+		
+	}
+	
+	public String getFirstName(){
+		return this.firstName;
+	}
+
+
+    public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String getLastName(){
+		return this.lastName;
+
+
+	} 
+    public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;	
+	}
+	
+	public String getEmailAddress(){
+		return this.emailAddress;
+	} 
+    
+    public void setAccountPassword(String accountPassword) {
+		this.accountPassword = accountPassword;
+	}
+	
+	public String getAccountPassword(){
+		return this.accountPassword;
+	} 
+    
+    public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	public String getPhoneNumber(){
+		return this.phoneNumber;
+	}
+
+    public void setAddress(String address){
+        this.address = address;
+    }
+
+    public String getAddress(){
+        return this.address;
+    }
+
+    public void setHealthCardNumber(String healthCardNumber){
+        this.healthCardNumber = healthCardNumber;
+    }
+
+    public String setHealthCardNumber(){
+        return this.healthCardNumber;
+    }
+
 
 }
