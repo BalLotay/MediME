a40917627d39d9d4f1749fe6444c2ed16eed9331
dguo095 <dguo095@uottawa.ca>
2023-09-27 18:17:17 -0400

    Add instances variables and complete constructor

--- Person.java
+++ Person.java
@@ -1,6 +1,21 @@
 public class Person { // Class created
-    private String name;
-    public Person(String name){
-        this.name = name;
+
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+    private String accountPassword;
+    private String phoneNumber;
+    private String address;
+    private int healthCardNumber;
+
+    public Person(String firstName, String lastName, String emailAddress, String accountPassword, String phoneNumber, String address, int healthCardNumber){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailAddress = emailAddress;
+        this.accountPassword = accountPassword;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.healthCardNumber = healthCardNumber;
     }
+
 }
