bbe2c349449b73608ea2436f11bb27510c7e0473
Balpreet Singh <singhbalpreet007@gmail.com>
2023-09-29 13:35:22 -0400

    Add constructor to Patient.java

---
+++ Patient.java
@@ -0,0 +1,7 @@
+public class Patient extends Person {
+
+    public Patient(String firstName, String lastName, String emailAddress, String accountPassword, String phoneNumber, String address, int healthCardNumber){
+        super(firstName, lastName, emailAddress, accountPassword, phoneNumber, address, healthCardNumber);
+    }
+
+}
