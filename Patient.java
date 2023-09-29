3954fec19afde2106dc78f771f4b8e54c9e80851
Balpreet Singh <singhbalpreet007@gmail.com>
2023-09-29 16:59:56 -0400

    Add healthCardNumber variable

--- Patient.java
+++ Patient.java
@@ -1,7 +1,11 @@
 public class Patient extends Person {
 
+    private int healthCardNumber;
+
     public Patient(String firstName, String lastName, String emailAddress, String accountPassword, String phoneNumber, String address, int healthCardNumber){
         super(firstName, lastName, emailAddress, accountPassword, phoneNumber, address, healthCardNumber);
+        this.healthCardNumber = healthCardNumber;
     }
+
 
 }
