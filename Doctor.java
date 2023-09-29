c7f72acc6b6dc9d9404c8815f79a832eb0d127cb
Balpreet Singh <singhbalpreet007@gmail.com>
2023-09-29 16:58:02 -0400

    Add constructor to Doctor.java

--- Doctor.java
+++ Doctor.java
@@ -1,7 +1,10 @@
 public class Doctor extends Person {
 
-    public Doctor(String firstName, String lastName, String emailAddress, String accountPassword, String phoneNumber, String address, int healthCardNumber){
+    private String[] specialties;
+
+    Doctor(String firstName, String lastName, String emailAddress, String accountPassword, String phoneNumber, String address, int healthCardNumber, String ...specialties){
         super(firstName, lastName, emailAddress, accountPassword, phoneNumber, address, healthCardNumber);
+        this.specialties = specialties;
     }
 
 }
