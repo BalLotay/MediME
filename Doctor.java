78afc1b1d7555149e1c5d14dadfd712465f827ad
Balpreet Singh <singhbalpreet007@gmail.com>
2023-09-29 17:01:51 -0400

    Remove healthCardNumber from constructor

--- Doctor.java
+++ Doctor.java
@@ -3,7 +3,7 @@ public class Doctor extends Person {
     private String[] specialties;
 
     Doctor(String firstName, String lastName, String emailAddress, String accountPassword, String phoneNumber, String address, int healthCardNumber, String ...specialties){
-        super(firstName, lastName, emailAddress, accountPassword, phoneNumber, address, healthCardNumber);
+        super(firstName, lastName, emailAddress, accountPassword, phoneNumber, address);
         this.specialties = specialties;
     }
 
--- Patient.java
+++ Patient.java
@@ -3,7 +3,7 @@ public class Patient extends Person {
     private int healthCardNumber;
 
     public Patient(String firstName, String lastName, String emailAddress, String accountPassword, String phoneNumber, String address, int healthCardNumber){
-        super(firstName, lastName, emailAddress, accountPassword, phoneNumber, address, healthCardNumber);
+        super(firstName, lastName, emailAddress, accountPassword, phoneNumber, address);
         this.healthCardNumber = healthCardNumber;
     }
 
