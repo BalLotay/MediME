0acb6e98af2b9121e29bc50a31fa1e66381fa4b3
Balpreet Singh <singhbalpreet007@gmail.com>
2023-10-27 21:57:18 -0400

    Add employeeNumber in constructor

--- FinalProject/LoginApp/app/src/main/java/com/example/loginapp/Doctor.java
+++ FinalProject/LoginApp/app/src/main/java/com/example/loginapp/Doctor.java
@@ -4,11 +4,13 @@ import java.util.Arrays;
 public class Doctor extends Person {
 
     private String[] specialties;
+    private int employeeNumber
 
     private String status;
 
-    public Doctor(String firstName, String lastName, String emailAddress, String accountPassword, String phoneNumber, String address, int healthCardNumber, String status, String ...specialties){
+    public Doctor(String firstName, String lastName, String emailAddress, String accountPassword, String phoneNumber, String address, int employeeNumber, String status, String ...specialties){
         super(firstName, lastName, emailAddress, accountPassword, phoneNumber, address);
+        this.employeeNumber = employeeNumber;
         this.specialties = specialties;
         this.status = status;
     }
@@ -19,6 +21,13 @@ public class Doctor extends Person {
 
     public void setStatus(String status){ // status can either be "pending", "accepted", or "declined"
         this.status = status;
+    }
+    public int getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    public void setEmployeeNumber(int newEmployeeNumber) {
+        employeeNumber = newEmployeeNumber;
     }
     public String getSpecialties() {
         return Arrays.toString(specialties);
