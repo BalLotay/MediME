8d7e48d9ad07d0fd493f597064cdec6b96d3018c
Balpreet Singh <singhbalpreet007@gmail.com>
2023-10-28 14:26:09 -0400

    Remove unnecessary imports

--- FinalProject/LoginApp/app/src/main/java/com/example/loginapp/MainActivityViewRejectedApplicants.java
+++ FinalProject/LoginApp/app/src/main/java/com/example/loginapp/MainActivityViewRejectedApplicants.java
@@ -5,11 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.firebase.database.DataSnapshot;
