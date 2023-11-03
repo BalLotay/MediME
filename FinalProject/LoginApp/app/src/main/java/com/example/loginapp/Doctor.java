e0f9576703df7dd9fc1eadd0b2a768e3edcdc074
dguo095 <144740389+dguo095@users.noreply.github.com>
2023-11-03 09:29:08 -0400

    added auto accept status

--- FinalProject/LoginApp/app/src/main/java/com/example/loginapp/Doctor.java
+++ FinalProject/LoginApp/app/src/main/java/com/example/loginapp/Doctor.java
@@ -7,12 +7,21 @@ public class Doctor extends Person {
     private int employeeNumber;
 
     private String status;
+    private boolean autoAcceptStatus;
 
     public Doctor(String firstName, String lastName, String emailAddress, String accountPassword, String phoneNumber, String address, int employeeNumber, String status, String ...specialties){
         super(firstName, lastName, emailAddress, accountPassword, phoneNumber, address);
         this.employeeNumber = employeeNumber;
         this.specialties = specialties;
         this.status = status;
+    }
+
+    public void setAutoAcceptStatus(boolean autoAcceptStatus){
+        this.autoAcceptStatus = autoAcceptStatus;
+    }
+
+    public boolean getAutoAcceptStatus(){
+        return autoAcceptStatus;
     }
 
     public String getStatus(){
