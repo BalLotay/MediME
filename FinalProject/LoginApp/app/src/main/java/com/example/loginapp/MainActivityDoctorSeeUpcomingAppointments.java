4c2a95b598d4e86ce04e67cef7a6affc37748954
Balpreet Singh <singhbalpreet007@gmail.com>
2023-11-06 12:19:09 -0500

    Add Doctor files

---
+++ FinalProject/LoginApp/app/src/main/java/com/example/loginapp/MainActivityDoctorSeeUpcomingAppointments.java
@@ -0,0 +1,13 @@
+package com.example.loginapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityDoctorSeeUpcomingAppointments extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_doctor_see_upcoming_appointments);
+    }
+}
