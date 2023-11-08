c4f514d400ddac88dc8499711759610c0211b483
stevenyangUO <145710260+stevenyangUO@users.noreply.github.com>
2023-11-07 22:14:28 -0500

    Add Shift and small fixes to Appointment and Doctor (Steven)

---
+++ FinalProject/LoginApp/app/src/main/java/com/example/loginapp/Shift.java
@@ -0,0 +1,45 @@
+package com.example.loginapp;
+
+import java.util.Date;
+public class Shift {
+
+    Date date;
+
+    String startTime;
+
+    String endTime;
+
+    public Shift(){
+
+    }
+
+    public Shift(Date date, String startTime, String endTime){
+        this.date = date;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+}
