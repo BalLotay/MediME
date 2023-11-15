71a3eb7aaea85abac73728cad75507a4bd972a0a
Balpreet Singh <singhbalpreet007@gmail.com>
2023-11-15 10:30:05 -0500

    Add missing code from shift branch

--- FinalProject/LoginApp/app/src/main/java/com/example/loginapp/Shift.java
+++ FinalProject/LoginApp/app/src/main/java/com/example/loginapp/Shift.java
@@ -1,29 +1,35 @@
 package com.example.loginapp;
 
 import java.util.Date;
+import java.util.Calendar;
 public class Shift {
 
-    Date date;
+    String date;
 
     String startTime;
 
     String endTime;
 
+    String doctor;
+
     public Shift(){
 
     }
 
-    public Shift(Date date, String startTime, String endTime){
+
+
+    public Shift(String date, String startTime, String endTime, String doctor){
         this.date = date;
         this.startTime = startTime;
         this.endTime = endTime;
+        this.doctor = doctor;
     }
 
-    public Date getDate() {
+    public String getDate() {
         return date;
     }
 
-    public void setDate(Date date) {
+    public void setDate(String date) {
         this.date = date;
     }
 
@@ -41,5 +47,52 @@ public class Shift {
 
     public void setEndTime(String endTime) {
         this.endTime = endTime;
+    }
+
+    public String getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(String doctor) {
+        this.doctor = doctor;
+    }
+
+    public boolean isValid(){
+        int end = 0;
+        int start = 0;
+        try {
+            start = Integer.parseInt(this.startTime);
+            end = Integer.parseInt(this.endTime);
+        }
+        catch (Exception e){
+            return false;
+        }
+
+        if(end <= start){
+            return false;
+        }
+        else{
+            return true;
+        }
+
+    }
+    public boolean conflictsWith(Shift shift2){
+        String date2 = shift2.getDate(); //"10/11/2023"
+        String[] date2Info = date2.split("/");
+        String[] date1Info = date.split("/");
+
+        int start1 = Integer.parseInt(this.startTime);
+        int end1 = Integer.parseInt(this.endTime);
+        int start2 = Integer.parseInt(shift2.getStartTime());
+        int end2 = Integer.parseInt(shift2.getEndTime());
+
+        if(date1Info[0].equals(date2Info[0]) && date1Info[1].equals(date2Info[1]) && date1Info[2].equals(date2Info[2])){
+            if ((start1 >= start2 && start1 <= end2) || (end1 >= start2 && end1 <= end2) || (start2 >= start1 && start2 <= end1) || (end2 >= start1 && end2 <= end1)){
+                return true;
+            }
+        }
+
+        return false;
+
     }
 }
