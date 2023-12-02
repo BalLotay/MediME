c3e75d37aea034c83cc8a1e1b831a4fe9088b59b
Balpreet Singh <singhbalpreet007@gmail.com>
2023-12-02 12:05:51 -0500

    Add method to see if appointment is cancellable

--- FinalProject/LoginApp/app/src/main/java/com/example/loginapp/Appointment.java
+++ FinalProject/LoginApp/app/src/main/java/com/example/loginapp/Appointment.java
@@ -1,92 +1,117 @@
 package com.example.loginapp;
+
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 
 import com.google.firebase.database.Exclude;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class Appointment {
-	String status; // pending, approved or cancelled
-	String date;
-	String startTime; // eg, "13:00"
-	String endTime;
-	String doctor; // Doctor's username (firstName)
-	String patient;
+    String status; // pending, approved or cancelled
+    String date;
+    String startTime; // eg, "13:00"
+    String endTime;
+    String doctor; // Doctor's username (firstName)
+    String patient;
 
-	public Appointment() {
+    public Appointment() {
 
-	}
+    }
 
-	public Appointment(String patient, String doctor, String date, String startTime, String endTime) {
-		status = "pending";
-		this.patient = patient;
-		this.doctor = doctor;
-		this.date = date;
-		this.startTime = startTime;
-		this.endTime = endTime;
-	}
+    public Appointment(String patient, String doctor, String date, String startTime, String endTime) {
+        status = "pending";
+        this.patient = patient;
+        this.doctor = doctor;
+        this.date = date;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
 
-	public String getDoctor() {
-		return doctor;
-	}
+    public String getDoctor() {
+        return doctor;
+    }
 
-	public String getPatient() {
-		return patient;
-	}
+    public String getPatient() {
+        return patient;
+    }
 
-	public String getStatus() {
-		return status;
-	}
+    public String getStatus() {
+        return status;
+    }
 
-	public void setStatus(String s) {
-		this.status = s;
-	}
+    public void setStatus(String s) {
+        this.status = s;
+    }
 
-	public String getEndTime() {
-		return endTime;
-	}
+    public String getEndTime() {
+        return endTime;
+    }
 
-	public void setEndTime(String e) {
-		this.endTime = e;
-	}
+    public void setEndTime(String e) {
+        this.endTime = e;
+    }
 
-	public String getStartTime() {
-		return startTime;
-	}
+    public String getStartTime() {
+        return startTime;
+    }
 
-	public void setStartTime(String s) {
-		this.startTime = s;
-	}
+    public void setStartTime(String s) {
+        this.startTime = s;
+    }
 
-	public String getDate() {
-		return this.date;
-	}
+    public String getDate() {
+        return this.date;
+    }
 
-	public void setDate(String d) {
-		this.date = d;
-	}
+    public void setDate(String d) {
+        this.date = d;
+    }
 
-	@Exclude
-	public boolean isPastAppointment(){
+    @Exclude
+    public boolean isPastAppointment() {
 
-		if (date == null || startTime == null) {
-			return false;
-		}
+        if (date == null || startTime == null) {
+            return false;
+        }
 
-		try {
-			return new SimpleDateFormat("MM/dd/yyyy").parse(date).before(new Date())
-					&& new SimpleDateFormat("HHmm").parse(startTime).before(new Date());
-		} catch (ParseException pe) {
-			return false;
-		}
-	}
+        String dateTimeStr = date + " " + startTime;
 
-	@NonNull
-	@Override
-	public String toString() {
-		return "status: " + status + ", date: " + date + ", startTime: " + startTime + ", endTime: " + endTime
-				+ ", patient: " + patient + ", doctor: " + doctor;
-	}
+        try {
+            return new SimpleDateFormat("MM/dd/yyyy HHmm").parse(dateTimeStr).before(new Date());
+        } catch (ParseException pe) {
+            return false;
+        }
+    }
+
+    @Exclude
+    public boolean isCancellableAppointment() {
+
+        if (date == null || startTime == null) {
+            return false;
+        }
+
+        String dateTimeStr = date + " " + startTime;
+
+        try {
+            Date d = new Date();
+            d.setTime(d.getTime() + TimeUnit.HOURS.toMillis(1));
+
+            return new SimpleDateFormat("MM/dd/yyyy HHmm").parse(dateTimeStr).after(d);
+        } catch (ParseException pe) {
+            return false;
+        }
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "status: " + status + ", date: " + date + ", startTime: " + startTime + ", endTime: " + endTime
+                + ", patient: " + patient + ", doctor: " + doctor;
+    }
 }
